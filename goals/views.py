44dd18170ab3e6cef71557f26f1327d3a67d4bc4
tduval <duval.thomas01@gmail.com>
2018-05-11 08:02:18 +0000

    add check to prevent user to access other user's goals page

--- goals/views.py
+++ goals/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime
 from django.contrib.auth.decorators import login_required
@@ -9,13 +9,16 @@ from .models import Track
 # index pages/default page displayed all Goals
 @login_required(login_url='/accounts/login/')
 def index(request):
-    goal_list = Goal.objects.all()
+    goal_list = Goal.objects.filter(owner=request.user)
     return render(request, 'goals/index.html', {'goal_list': goal_list})
 
+# Checks if the authenticvated user try to access their own goals and not someone else goals page
 # detail page of a specific goal with all his associated Tracks
 @login_required(login_url='/accounts/login/')
 def goal(request, goal_id):
     goal = Goal.objects.get(id=goal_id)
+    if not goal.owner == request.user:
+        return redirect('goals:index')
     track_list = Track.objects.filter(goal=goal_id)
     return render(request, 'goals/goal.html', {'goal': goal, 'track_list': track_list})
 
