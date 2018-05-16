b223634b1878c9b134c46b9d0075b03fc9cc2ba6
tduval <duval.thomas01@gmail.com>
2018-05-16 02:21:36 +0000

    add fields to display on Admin seciton for both Goals (mean score) and tracks

--- goals/admin.py
+++ goals/admin.py
@@ -1,17 +1,29 @@
 from django.contrib import admin
-
 from .models import Goal, Track
 
 # Register your models here.
 
+
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ('goal_text', 'owner', 'tracks_count', 'start_date', 'end_date')
+    list_display = ('goal_text', 'owner', 'tracks_count', 'start_date', 'end_date', 'tracks_mean')
 
     def tracks_count(self, obj):
         return obj.track_set.count()
 
+    def tracks_mean(self, obj):
+        tracks = obj.track_set.all()
+        if tracks.count() == 0:
+            return 0
+        else:
+            mean = 0
+            for t in tracks:
+                mean += t.track_score
+            return mean/(self.tracks_count(obj))
 
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('date', 'goal', 'track_score', 'comment_text')
 
 admin.site.register(Goal, GoalAdmin)
-admin.site.register(Track)
+admin.site.register(Track, TrackAdmin)
 
--- goals/models.py
+++ goals/models.py
@@ -20,4 +20,4 @@ class Track(models.Model):
     comment_text = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.date
+        return self.date.strftime('%d/%m/%Y')
