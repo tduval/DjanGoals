eb87ae47d5855584e1e9334e31ce1770f370fe09
tduval <duval.thomas01@gmail.com>
2018-05-16 01:49:51 +0000

    add track count fields to display on admin Goals section

--- goals/admin.py
+++ goals/admin.py
@@ -5,7 +5,12 @@ from .models import Goal, Track
 # Register your models here.
 
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ('goal_text', 'owner', 'start_date', 'end_date')
+    list_display = ('goal_text', 'owner', 'tracks_count', 'start_date', 'end_date')
+
+    def tracks_count(self, obj):
+        return obj.track_set.count()
+
+
 
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(Track)
